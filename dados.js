50844b63e7644c521d257a26c955a0004b911878
carlospenteado <carlos_metz@hotmail.com>
2024-09-06 17:17:31 -0300

    atualizado código

--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let dados = [
     {
         titulo: "Gabriel Medina",
         descricao: "Gabriel Medina é um dos maiores nomes do surf mundial. Nascido em São Sebastião, litoral norte de São Paulo, ele conquistou o coração dos brasileiros com suas manobras radicais e títulos expressivos. Com três títulos mundiais, Medina se tornou o primeiro brasileiro a alcançar esse feito histórico no surf profissional. Sua habilidade em dominar as ondas gigantes e sua personalidade carismática o transformaram em um verdadeiro ídolo nacional e inspiração para jovens surfistas. Além de seus feitos no surf, Medina também é conhecido por sua paixão pelo Brasil e por sua luta por causas sociais.",
-        link: "https://pt.wikipedia.org/wiki/Gabriel_Medina"
+        link: "https://pt.wikipedia.org/wiki/Gabriel_Medina",
+        tags: "surfista medina Gabriel Medina Pinto Ferreira"
     },
     {
         titulo: "Neco Padaratz",
         descricao: "Neco Padaratz é um dos maiores nomes do surf brasileiro, conhecido por sua técnica apurada e sua rivalidade com o irmão Teco Padaratz. Ele conquistou dois títulos mundiais da World Qualifying Series (WQS) e é considerado um dos surfistas mais influentes do país.",
-        link: "https://pt.wikipedia.org/wiki/Neco_Padaratz"
+        link: "https://pt.wikipedia.org/wiki/Neco_Padaratz",
+        tags: "surfista percy Percy"
     },
     {
         titulo: "Picuruta Salazar",
         descricao: "Picuruta Salazar é um ícone do surf brasileiro, conhecido por sua personalidade carismática e sua habilidade em ler as ondas. Considerado um dos maiores surfistas da história do Brasil, Picuruta é uma referência para diversas gerações de surfistas.",
-        link: "https://pt.wikipedia.org/wiki/Picuruta_Salazar" 
+        link: "https://pt.wikipedia.org/wiki/Picuruta_Salazar",
+        tags: "surfista picuruta miau gato" 
     }
 ];
